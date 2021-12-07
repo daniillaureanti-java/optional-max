4c7e0e630f9f58f2fff733fd104740f399686fc9
Daniil Laureanti <daniil.laureanti@gmail.com>
2021-12-07 14:33:19 +0200

    Completed task

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -1,9 +1,23 @@
 package com.epam.rd.autotasks.max;
 
 import java.util.OptionalInt;
+import java.util.stream.IntStream;
 
 public class MaxMethod {
     public static OptionalInt max(int[] values) {
-        throw new UnsupportedOperationException();
+        OptionalInt opInt = OptionalInt.empty();
+
+        if (values == null){
+            return opInt;
+        }
+
+        IntStream intStream = IntStream.of(values);
+        OptionalInt res = intStream.max();
+
+        if (res.isPresent()) {
+            return opInt = OptionalInt.of(res.getAsInt());
+        } else {
+            return opInt;
+        }
     }
 }
